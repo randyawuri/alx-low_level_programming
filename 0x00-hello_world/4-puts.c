b4cb994635564e04b01f372bc00def177eb3a826
Randy <awuri.randy@yahoo.com>
2023-07-13 14:48:40 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  * main - this is the starting point of the program.
  * Return: 0, success.
 */
-int main() {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
