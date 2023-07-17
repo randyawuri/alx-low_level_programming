c639b1c94015a9c6aa3d2cb7dedb651779739003
Randy <awuri.randy@yahoo.com>
2023-07-17 15:48:29 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,9 +9,10 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar(' ');
-		putchar(',');
 		putchar('0' + i);
+		if (i != 9)
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
